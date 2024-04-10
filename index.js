d929980e0a5bfda9ad936d50a9e122953adc5e68
Inesh-Reddy <ineshreddy007@gmail.com>
2024-04-10 09:55:15 +0530

    removing comments

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
-const tasks = require("./routes/tasks"); // this is a middleware
+const tasks = require("./routes/tasks"); 
 const port =3000;
 const connectDB = require('./db/connection');
 require('dotenv').config()
 
-// middleware
+
 app.use(express.json());
 
-// routes
+
 app.get("/", (req, res)=>{
     res.status(200);
     res.send("Task Manager App");
 })
-app.use('/api/v1/tasks', tasks); //when anything hits at '/api/v1/tasks--->tasks middleware is called'
-
+app.use('/api/v1/tasks', tasks);
 
 const start = async()=>{
     try {
