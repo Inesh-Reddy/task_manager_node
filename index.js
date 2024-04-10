42867272e18dd234c8080dd5939e0dd9a139efc1
Inesh-Reddy <ineshreddy007@gmail.com>
2024-04-10 09:52:41 +0530

    setting up mongoDB connection

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const tasks = require("./routes/tasks"); // this is a middleware
 const port =3000;
+const connectDB = require('./db/connection');
+require('dotenv').config()
 
-
+// middleware
 app.use(express.json());
+
 // routes
 app.get("/", (req, res)=>{
     res.status(200);
@@ -12,6 +15,15 @@ app.get("/", (req, res)=>{
 })
 app.use('/api/v1/tasks', tasks); //when anything hits at '/api/v1/tasks--->tasks middleware is called'
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}...`);
-})
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, ()=>{
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(`error`);
+    }
+}
+start();
